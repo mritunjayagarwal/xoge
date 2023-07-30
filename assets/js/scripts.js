a775195030de9c05ed14554d010e5e4af35b39c2
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-07-30 18:08:23 +0530

    kk

---
+++ assets/js/scripts.js
@@ -0,0 +1,165 @@
+/*===================================
+Author       : Bestwebcreator.
+Template Name: Cryptoking - Bitcoin & ICO Cryptocurrency Landing Page HTML Template
+Version      : 1.5
+===================================*/
+
+/*===================================*
+LANDING PAGE JS
+*===================================*/
+
+(function($) {
+	'use strict';
+   
+	/*===================================*
+	04. BACKGROUND ANIMATION JS
+	*===================================*/
+	var $particles_js = $('#banner_bg_effect');
+	if ($particles_js.length > 0) {
+		particlesJS('banner_bg_effect',
+			// Update your personal code.
+			{
+				"particles": {
+					"number": {
+						"value": 80,
+						"density": {
+							"enable": true,
+							"value_area": 800
+						}
+					},
+					"color": {
+						"value": "#fff"
+					},
+					"shape": {
+						"type": "polygon",
+						"stroke": {
+							"width": 0,
+							"color": "#000000"
+						},
+						"polygon": {
+							"nb_sides": 5
+						},
+						"image": {
+							"src": "img/github.svg",
+							"width": 100,
+							"height": 100
+						}
+					},
+					"opacity": {
+						"value": 0.4,
+						"random": false,
+						"anim": {
+							"enable": false,
+							"speed": 1,
+							"opacity_min": 0.1,
+							"sync": false
+						}
+					},
+					"size": {
+						"value": 3,
+						"random": true,
+						"anim": {
+							"enable": false,
+							"speed": 40,
+							"size_min": 0.1,
+							"sync": false
+						}
+					},
+					"line_linked": {
+						"enable": true,
+						"distance": 150,
+						"color": "#fff",
+						"opacity": 0.1,
+						"width": 1
+					},
+					"move": {
+						"enable": true,
+						"speed": 6,
+						"direction": "none",
+						"random": false,
+						"straight": false,
+						"out_mode": "out",
+						"bounce": false,
+						"attract": {
+							"enable": false,
+							"rotateX": 600,
+							"rotateY": 1200
+						}
+					}
+				},
+				"interactivity": {
+					"detect_on": "canvas",
+					"events": {
+						"onhover": {
+							"enable": true,
+							"mode": "repulse"
+						},
+						"onclick": {
+							"enable": true,
+							"mode": "push"
+						},
+						"resize": true
+					},
+					"modes": {
+						"grab": {
+							"distance": 400,
+							"line_linked": {
+								"opacity": 1
+							}
+						},
+						"bubble": {
+							"distance": 400,
+							"size": 40,
+							"duration": 2,
+							"opacity": 8,
+							"speed": 3
+						},
+						"repulse": {
+							"distance": 200,
+							"duration": 0.4
+						},
+						"push": {
+							"particles_nb": 4
+						},
+						"remove": {
+							"particles_nb": 2
+						}
+					}
+				},
+				"retina_detect": true
+			}
+	
+		);
+	}
+	
+	  
+  	/*===================================*
+	05. BACKGROUND ANIMATION JS
+	*===================================*/
+	 $('.roadmap').owlCarousel({
+	     loop: false,
+	     margin: 30,
+	     nav: true,
+	     navText: ['<i class="ion-ios-arrow-back"></i>', '<i class="ion-ios-arrow-forward"></i>'],
+	     responsive: {
+	         0: {
+	             items: 1,
+
+	         },
+	         380: {
+	             items: 2,
+	             margin: 15,
+	         },
+	         600: {
+	             items: 3
+	         },
+	         1000: {
+	             items: 5
+	         },
+	         1199: {
+	             items: 5
+	         }
+	     }
+	 });	
+})(jQuery);
+
